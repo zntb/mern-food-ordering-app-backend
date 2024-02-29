5d892f27b00dc5db52fc648d64db38ae155fb134
zntb <zenetibi@yahoo.com>
2024-02-29 16:16:21 +0200

    Validate Access Token

--- src/routes/myUserRoute.ts
+++ src/routes/myUserRoute.ts
@@ -1,9 +1,10 @@
 import express from 'express';
 import myUserController from '../controllers/myUserController';
+import { jwtCheck } from '../middleware/auth';
 
 const router = express.Router();
 
 // /api/my/user
-router.route('/').post(myUserController.createCurrentUser);
+router.route('/').post(jwtCheck, myUserController.createCurrentUser);
 
 export default router;
